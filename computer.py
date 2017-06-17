17f256cdd80f15fab68d325f9ea2d8462c2b8f6b
nsobolewska <n_sobolewska@wp.pl>
2017-06-17 22:46:01 +0200

    Drawing plot on googlemap.

--- computer.py
+++ computer.py
@@ -1,10 +1,26 @@
 import socket
+import gmplot
+import time
+import googlemaps
+from datetime import datetime
+
+# googleapi key: AIzaSyD80pxex1anVGvFuK47zOpN2owAoTyE9HM
 
 host="192.168.8.100"
 port=5005
 s=socket.socket()
 s.bind((host, port))
 s.listen(10)
+
+def googlemap():
+
+    latitudes = [54.5496054,54.5496054,54.5496912599,54.5496912599]
+    longitudes = [18.5462068,18.5462068,18.5463277236,18.5463277236]
+
+    gmap = gmplot.GoogleMapPlotter(37.428, -122.145, 16)
+
+    gmap.plot(latitudes, longitudes, 'cornflowerblue', edge_width=10)
+    gmap.draw("mymap.html")
 
 def server():
         while True:
@@ -17,4 +33,5 @@ def server():
                 else:
                         print("-> "+ decoded_data + "\n")
 
-server()
+# server()
+googlemap()
