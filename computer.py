9cc0d946e739431d2e7d2958d45bbcc1fbeea28f
nsobolewska <n_sobolewska@wp.pl>
2017-06-17 22:58:51 +0200

    Drawing plot on googlemap.

--- computer.py
+++ computer.py
@@ -17,13 +17,9 @@ def googlemap():
     latitudes = [54.5496054,54.5496054,54.5496912599,54.5496912599]
     longitudes = [18.5462068,18.5462068,18.5463277236,18.5463277236]
 
-    gmap = gmplot.GoogleMapPlotter(37.428, -122.145, 16)
+    gmap = gmplot.GoogleMapPlotter(latitudes[0], longitudes[0], 16)
 
     gmap.plot(latitudes, longitudes, 'cornflowerblue', edge_width=10)
-    #
-    # gmap.scatter(more_lats, more_lngs, '#3B0B39', size=40, marker=False)
-    # gmap.scatter(marker_lats, marker_lngs, 'k', marker=True)
-    # gmap.heatmap(heat_lats, heat_lngs)
     gmap.draw("mymap.html")
 
 def server():
