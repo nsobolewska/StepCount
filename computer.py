5b51e27399e3e4230f83646636c78ad471c3d3ee
nsobolewska <n_sobolewska@wp.pl>
2017-06-18 14:12:14 +0200

    Added buttons to start accelerometer and gps

--- computer.py
+++ computer.py
@@ -1,5 +1,8 @@
 import socket
 import gmplot
+import numpy as np
+import matplotlib.pyplot as mpl
+from scipy import signal
 import time
 import googlemaps
 from datetime import datetime
@@ -7,13 +10,63 @@ from datetime import datetime
 # googleapi key: AIzaSyD80pxex1anVGvFuK47zOpN2owAoTyE9HM
 
 host="192.168.8.100"
-port=5005
+port=3000
 s=socket.socket()
 s.bind((host, port))
 s.listen(10)
 
+def akcelerometr():
+    with open("akcelerometr.txt", "r") as ins:
+        akc = []
+        for line in ins:
+            for word in line.split():
+                akc.append(word)
+    akcX = []
+    akcY = []
+    akcZ = []
+    i = 0
+    while i < len(akc):
+        akcX.append(akc[i])
+        akcY.append(akc[i + 1])
+        akcZ.append(akc[i + 2])
+        i += 3
+
+    Z = np.zeros(len(akcZ))
+    for j in range(len(akcZ)):
+        Z[j] = float(akcZ[j])-9
+    time = np.ones(len(akcX))
+    time = np.cumsum(time)
+    # time = time/24
+    mag = np.zeros(len(akcX))
+    for j in range(len(akcX)):
+        mag[j] = np.sqrt((float)(akcX[j])**2 + (float)(akcY[j])**2 + (float)(akcZ[j])**2)
+
+    magNoG = mag - np.mean(mag)
+
+    minPeakHeight = np.std(magNoG)
+
+    maxPeakHeight = minPeakHeight + 1
+
+    pks = signal.find_peaks_cwt(magNoG,np.arange(1,6))
+
+    kroki = 0
+    for loc in pks:
+        if magNoG[loc]>minPeakHeight and magNoG[loc]<maxPeakHeight :
+            kroki+=1
+
+    print("Ilosc krokow",kroki)
+
+    mpl.plot(time,magNoG)
+    mpl.show()
+
 def googlemap():
 
+    with open("gps.txt", "r") as ins:
+        gps = []
+        for line in ins:
+            for word in line.split():
+                gps.append(word)
+    print(gps)
     latitudes = [54.5496054,54.5496054,54.5496912599,54.5496912599]
     longitudes = [18.5462068,18.5462068,18.5463277236,18.5463277236]
 
@@ -33,5 +86,5 @@ def server():
                 else:
                         print("-> "+ decoded_data + "\n")
 
-# server()
-googlemap()
+akcelerometr()
+# googlemap()
--- phone.py
+++ phone.py
@@ -14,42 +14,61 @@ class UI(FloatLayout):#the app ui
 	tekst = ""
 	tekst2 = ""
 	clic = 0
+	names = 0
 	def __init__(self, **kwargs):
 		super(UI, self).__init__(**kwargs)
-		layout = GridLayout(cols=1,rows=4, row_force_default=True, row_default_height=300)
+		layout = GridLayout(cols=1,rows=5, row_force_default=True, row_default_height=200)
 		self.lblAcce = Label(text="Accelerometer: ",width=100,height=20) #create a label at the center
-		self.lblAcce2 = Label(text="Gps: ",width=100,height=20) #create a label at the center
 		self.btn = Button(text="Zapisz", width=50,height=50)
 		self.btn.bind(on_press=self.save)
 		self.btn2 = Button(text="Zakoncz", width=50,height=50)
 		self.btn2.bind(on_press=self.koncz)
+		self.btn3 = Button(text="Start akcelerometr", width=50,height=50)
+		self.btn3.bind(on_press=self.tryos)
+		self.btn4 = Button(text="Start gps", width=50,height=50)
+		self.btn4.bind(on_press=self.gpstart)
 		layout.add_widget(self.btn)
 		layout.add_widget(self.btn2)
+		layout.add_widget(self.btn3)
+		layout.add_widget(self.btn4)
 		layout.add_widget(self.lblAcce)
-		layout.add_widget(self.lblAcce2)
 		self.add_widget(layout)
-		gps.configure(on_location=self.print_locations)
-		self.tryos()
+		# self.tryos()
+		Clock.schedule_interval(self.save, 180)
+
+	def gpstart(self,instance):
+		try:
+			gps.configure(on_location=self.print_locations)
+			gps.start()
+		except:
+			self.lblAcce.text = "Failed to start gps"  # error
+
+
 
 	def koncz(self,value):
 		sys.exit()
 
 	def print_locations(self,**kwargs):
-		self.tekst2 = self.tekst2+ 'lat: {lat}, lon: {lon}'.format(**kwargs)
+		self.tekst2 = self.tekst2+ '{lat} {lon}'.format(**kwargs) #latitude longitude
 
 	def save(self,instance):
 		#--------------ZAPISZ DO PLIKU--------------------#
-		file2write = open("akcelerometr.txt", 'w')
+		nameOfFileA = "akcelerometr%.0f.txt"%self.names
+		nameOfFileG = "gps%.0f.txt" % self.names
+		file2write = open(nameOfFileA, 'w')
 		file2write.write(self.tekst)
 		file2write.close()
-		file2write2 = open("gps.txt", 'w')
+		file2write2 = open(nameOfFileG, 'w')
 		file2write2.write(self.tekst2)
 		file2write2.close()
+		self.tekst = ""
+		self.tekst2 = ""
+		self.names+=1
 		#-------------------------------------------------#
 		# #-------------------------------------------------#
 		# #-----------SOCKET--------------------------------#
 		# host = "192.168.8.100"
-		# port = 5005
+		# port = 3000
 		# self.s = socket.socket()
 		# self.s.connect((host, port))
 		# self.tekst = "Koniec" #self.tekst +
@@ -60,20 +79,18 @@ class UI(FloatLayout):#the app ui
 		# 	self.sock.send(str.encode(line))
 		# 	time.sleep(1/20)
 
-	def tryos(self):
-		if self.clic == 0:
-			try:
-				accelerometer.enable() #enable the accelerometer
-				gps.start()
-				#if you want do disable it, just run: accelerometer.disable()
-				Clock.schedule_interval(self.update, 1.0/24) #24 calls per second
-			except:
-				self.lblAcce.text = "Failed to start accelerometer" #error
+	def tryos(self,instance):
+		try:
+			accelerometer.enable() #enable the accelerometer
+			#if you want do disable it, just run: accelerometer.disable()
+			Clock.schedule_interval(self.update, 1.0/15) #24 calls per second
+		except:
+			self.lblAcce.text = "Failed to start accelerometer" #error
 
 	def update(self, dt):
 		txt = ""
 		try:
-			txt = "Accelerometer:\nX = %.2f\nY = %.2f\nZ = %2.f " %(
+			txt = "Accelerometer:\nX = %.2f\nY = %.2f\nZ = %.2f " %(
 				accelerometer.acceleration[0], #read the X value
 				accelerometer.acceleration[1], # Y
 				accelerometer.acceleration[2]) # Z
@@ -81,7 +98,7 @@ class UI(FloatLayout):#the app ui
 		except:
 			txt = "Cannot read accelerometer!" #error
 		try:
-			self.tekst = self.tekst + "%.2f	%.2f	%2.f\n"%(		#x,y,z from accelerometer
+			self.tekst = self.tekst + "%.2f	%.2f	%.2f\n"%(		#x,y,z from accelerometer
 			accelerometer.acceleration[0], #read the X value
 			accelerometer.acceleration[1], # Y
 			accelerometer.acceleration[2]) # Z
